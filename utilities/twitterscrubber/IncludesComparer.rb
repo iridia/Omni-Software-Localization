1bfe01fd56fcd23e1e9e614cb760a6a9436f05c7
Derek Hammer <derek.r.hammer@gmail.com>
2009-10-20 12:21:06 -0400

    Remove twitterscrubber

--- utilities/twitterscrubber/IncludesComparer.rb
+++
@@ -1,9 +0,0 @@
-class IncludesComparer
-	def initialize(includedText)
-		@includedText = includedText
-	end
-	
-	def valid?(value)
-		return value.include?(@includedText)
-	end
-end
--- utilities/twitterscrubber/README.md
+++
@@ -1,3 +0,0 @@
-This tool is now depreciated.
-
-Please see utilities/EngJourneering
