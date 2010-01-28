0de06117bca73824b1db44717290d1e48c9c6a85
Chandler Kent <ckentster@gmail.com>
2010-01-28 00:55:25 -0500

    Fixing up the jake couch tasks and removing the now unnecessary shell script. t:0h30m

--- utilities/couch/setupFinds.sh
+++
@@ -1,9 +0,0 @@
-# This is a script to set up the "finder" namespace in CouchDB. Run this if you are missing some of those
-# This will NOT override existing "finder" namespaces!
-
-curl -T find -H "Content-Type: application/json" localhost:5984/error/_design/finder
-curl -T project -H "Content-Type: application/json" localhost:5984/project/_design/finder
-curl -T user -H "Content-Type: application/json" localhost:5984/user/_design/finder
-curl -T message -H "Content-Type: application/json" localhost:5984/message/_design/finder
-curl -T find -H "Content-Type: application/json" localhost:5984/glossary/_design/finder
-curl -T find -H "Content-Type: application/json" localhost:5984/feedback/_design/finder
